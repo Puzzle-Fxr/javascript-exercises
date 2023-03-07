a5ba358811a21bb860886f4091ef12d398ee4050
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-07 17:01:19 +0000

    complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    const splitWords = words.split("");
+    const rsplitWords = splitWords.reverse();
+    const reversedWords = rsplitWords.join("");
+    return reversedWords;
 };
-
+// console.log(reverseString("Hey! Vsauce!"));
 // Do not edit below this line
 module.exports = reverseString;
