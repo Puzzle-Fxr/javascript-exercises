facfcaf40d8fe5d2758cea33bd55a813ad3a3ab0
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-07 14:43:40 +0000

    complete tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+/*const repeatString = function(word, fig) {
+    return word.repeat(fig);
+};*/
 
+const repeatString = function(word, fig) {
+    if (fig < 0){
+        return "ERROR";
+    } else return word.repeat(fig);
 };
-
 // Do not edit below this line
 module.exports = repeatString;
