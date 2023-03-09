7ff72c17361452db2b60bc6f1575fc4d2ad32011
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-09 14:02:10 +0000

    complete ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...array2) {
+    let newArray = [];
+    array1.forEach((element) => {
+        if (!array2.includes(element)){
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
 
-};
 
+// console.log(removeFromArray([1, 2, 3, 4, 5, "yo"], 4, 3, "yo"));
 // Do not edit below this line
 module.exports = removeFromArray;
