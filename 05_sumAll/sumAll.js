218498fea596ef28bc9120dc09f3cf1145d7d043
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-21 19:52:57 +0000

    need to diff string numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,26 @@
 const sumAll = function(number1, number2) {
     let finalArr = [];
-    for (let i = number1; i < number2 + 1 ; i++){
-        finalArr.push(i);
-    }
     let total = 0;
-
-    for (var element of finalArr){
+    if (number1 > 0 && number2 > 0 && number1 < number2){
+        for (let i = number1; i < number2 + 1 ; i++){
+        finalArr.push(i);
+        }
+        for (var element of finalArr){
         total += element;
-    } 
-    return total;
+        } 
+        return total;
+    } else if (number1 > 0 && number2 > 0 && number1 > number2){
+        for (let i = number2; i < number1 + 1 ; i++){
+        finalArr.push(i);
+        }
+        for (var element of finalArr){
+        total += element;
+        } 
+        return total;
+    } else return "ERROR";
 };
-console.log(sumAll(1, 4));
+
+console.log(sumAll("1", 15));
+console.log(sumAll(2, 15));
 // Do not edit below this line
 module.exports = sumAll;
